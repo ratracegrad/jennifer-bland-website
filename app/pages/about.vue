d801c709e7e03fc059ed8d07f08fdfd3bde78de1
Jennifer Bland <ratracegrad@gmail.com>
2025-12-26 17:38:09 -0500

    Updated the about me page. Added image for about me page.

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -34,7 +34,7 @@ useSeoMeta({
       }"
     >
       <UColorModeAvatar
-        class="sm:rotate-4 size-36 rounded-lg ring ring-default ring-offset-3 ring-offset-(--ui-bg)"
+        class="sm:rotate-4 size-36 rounded-lg ring ring-default ring-offset-3 ring-offset-bg"
         :light="global.picture?.light!"
         :dark="global.picture?.dark!"
         :alt="global.picture?.alt!"
@@ -47,16 +47,7 @@ useSeoMeta({
     >
       <MDC
         :value="page.content"
-        unwrap="p"
       />
-      <div class="flex flex-row justify-center items-center py-10 space-x-[-2rem]">
-        <PolaroidItem
-          v-for="(image, index) in page.images"
-          :key="index"
-          :image="image"
-          :index
-        />
-      </div>
     </UPageSection>
   </UPage>
 </template>
